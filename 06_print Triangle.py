169279e3a5dd1365c30e9945859ba922758ea343
harshitg123 <harshit.28.sps@gmail.com>
2020-06-17 18:26:03 +0530

    Example-6 geeks for geeks

---
+++ 06_print Triangle.py
@@ -0,0 +1,20 @@
+# Program is for printing 
+# A Triangle
+
+# * 
+# *  * 
+# *  *  * 
+# *  *  *  * 
+# *  *  *  *  * 
+
+def  fun1(n): 
+    if (n >= 0): 
+        fun1(n - 1)  
+    for i in range(n): 
+        print(" * ",end="")
+    print("\n",end="")    
+fun1(5)        
+
+# I modified this question for printing 
+# Triangle but for you want a look then visit 
+# Geeks for Geeks :- https://www.geeksforgeeks.org/practice-questions-for-recursion-set-3/?ref=lbp
