99b46e32c2e7d2e865576806de5cd555908d203c
Harshit gupta <harshit.28.sps@gmail.com>
2020-06-19 19:20:46 +0530

    Example-02

--- 02_Factorial No.py
+++ 02_Factorial No.py
@@ -9,3 +9,21 @@ def fac(n):
 
 inp=int(input())
 print("Factorial of",inp,"is:",fac(inp))
+
+'''
+Map of the above problem: for n=5
+
+           fun(5)
+             /
+        n*fun(4)         n=5
+           /              ^
+      n*fun(3)           n=4
+         /                ^
+     n*fun(2)            n=3
+       /                  ^
+    n*fun(1)             n=2
+     /                    ^
+   n*fun(0)              n=1 
+   
+   factorial = 1*2*3*4*5 = 120 
+'''
