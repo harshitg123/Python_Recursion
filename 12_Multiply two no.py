2695089b5919cd81f62d272c033f55388959d356
harshitg123 <harshit.28.sps@gmail.com>
2020-06-19 13:16:47 +0530

    Example-12 Geeks for Geeks

---
+++ 12_Multiply two no.py
@@ -0,0 +1,15 @@
+# program for calculating 
+# multiplacation of two no.
+
+def mul(a,b):
+    if b==0:
+        return 0
+    if b%2==0:
+        return mul(a+a, b//2)
+    else:
+        return a+mul(a+a, b//2)
+        
+print(mul(int(input()),int(input())))        
+
+# for better understand visit Geeks for Geeks
+# https://www.geeksforgeeks.org/practice-questions-for-recursion-set-5/?ref=lbp
